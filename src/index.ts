1b4e8f3468ccc3eef31f5e3f768a2ab1091f4d1b
MathUlbrich <mathulbrich@gmail.com>
2021-06-01 16:48:46 -0300

    Fraudhub test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 import { GetFeedVideos } from './crawler';
 import { UpvoteAction } from './upvote';
@@ -6,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 const server = express();
 
 server.use(express.static('public'));
+server.use(bodyParser.json());
 
 server.get('/api/feed', async (req, res) => {
     res.json(await GetFeedVideos());
@@ -13,6 +15,18 @@ server.get('/api/feed', async (req, res) => {
 
 server.put('/api/upvote/:id', async (req, res) => {
     res.json(await UpvoteAction(req.params.id));
+});
+
+server.post('/fraudhub/friction', (req, res) => {
+    res.status(401).json(req.body);
+});
+
+server.post('/fraudhub/denial', (req, res) => {
+    res.status(403).json(req.body);
+});
+
+server.post('/fraudhub/invalid', (req, res) => {
+    res.status(422).json(req.body);
 });
 
 server.listen(PORT, () => {
